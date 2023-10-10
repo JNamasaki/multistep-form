799fff45b7112f27aab340b09e5f71677c03f476
JNamasaki(Jessica Rodrigues) <r.jmrsilva.j@gmail.com>
2023-10-10 15:39:15 -0300

    .

--- controller/sheet.js
+++ controller/sheet.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const { google } = require('googleapis')
 
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'credentials.json');
+const credentials = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+
 async function getAuthSheets() {
     const auth = new google.auth.GoogleAuth({
 
-        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        keyFile: filePath,
         scopes: "https://www.googleapis.com/auth/spreadsheets"
     });
 
