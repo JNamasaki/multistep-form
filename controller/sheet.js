84d21611bba5b24ec50897fb50c851d99ac03fdf
JNamasaki(Jessica Rodrigues) <r.jmrsilva.j@gmail.com>
2023-10-10 15:22:24 -0300

    credentials

--- controller/sheet.js
+++ controller/sheet.js
@@ -1,28 +1,29 @@
 const express = require('express');
 
-const { google} = require('googleapis')
+const { google } = require('googleapis')
 
- async function getAuthSheets(){
+async function getAuthSheets() {
     const auth = new google.auth.GoogleAuth({
-        keyFile:"./credentials.json",
-        scopes:"https://www.googleapis.com/auth/spreadsheets"
+
+        keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
     });
 
-      
 
-    
+
+
     const client = await auth.getClient();
-    
+
     const googleSheets = google.sheets({
         version: "v4",
-        auth:client,
+        auth: client,
 
     });
 
     const spreadsheetId = "16XW02uXluw3_SIJphrdmN2UX4kJ9cVdcGZPcmlmg-yY";
-    return{ auth,client,googleSheets, spreadsheetId }
+    return { auth, client, googleSheets, spreadsheetId }
 }
 
 module.exports = {
-   getAuthSheets
+    getAuthSheets
 }
